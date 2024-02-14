44d43f776fafc99fd8169037039459aa11ff76e0
Owais <owais753@gmail.com>
2024-02-14 12:57:37 +0530

    solved questions

--- functions.js
+++ functions.js
@@ -108,10 +108,105 @@ const sumOfArray = (arr) => {
     return arrSum
 
 }
-sumArr = [1,2,4,5,6]
+sumArr = [1, 2, 4, 5, 6]
 console.log(`The sum of Array is ${sumOfArray(sumArr)}`);
 
 // ---------------------------------------------------------------------------------------------------
+
+// Q7 - Write a function that takes a number as input and returns true if the number is even, and false otherwise.
+
+const isItEven = (num) => {
+    if (num % 2 == 0) {
+        return true
+    } else {
+        return false
+    }
+}
+
+console.log(isItEven(2)) //OUTPUT - True
+console.log(isItEven(3)) //OUTPUT - False
+
+// ---------------------------------------------------------------------------------------------------
+
+// Q8 - Create a function that takes an array of numbers as input and returns a new array with only the even numbers from the original array.
+
+const evenInArray = (arr) => {
+    const evenArray = [];
+
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 == 0) {
+            evenArray.push(arr[i])
+        }
+    }
+
+    return evenArray;
+
+}
+
+console.log(evenInArray([1, 2, 3, 4, 5, 6, 8])) // OUTPUT - [2, 4, 6, 8]
+
+// ---------------------------------------------------------------------------------------------------
+
+// Q9 - Write a function that takes two parameters: a string and a letter. The function should return the number of times the letter appears in the string.
+
+const letterAppereance = (string, letter) => {
+
+    string.split("");
+    let count = 0;
+
+    let newString = string.toLowerCase();
+    let newLetter = letter.toLowerCase();
+
+    for (i = 0; i < newString.length; i++) {
+        if (newLetter.length > 1 || newString.length < 2) {
+            console.log("invalid input of STRING OR LETTER");
+            break
+
+        } else if (newString[i] === newLetter) {
+            count++
+        }
+    }
+
+    if (count === 0) {
+        console.log("The letter is not present");
+    }
+
+    console.log(` Count of ${newLetter} is ${count} `);
+}
+
+letterAppereance("owaiIiis", "I") // OUTPUT -  Count of i is 4 
+
+// ---------------------------------------------------------------------------------------------------
+
+// Q10 - Create a function that takes two arrays as input and returns a new array containing elements that are present in both arrays.
+
+const similarElements = (arr1,arr2) => {
+
+    const similars= [];
+
+    for ( let i = 0; i < arr1.length; i++){
+        
+        for (let j = 0 ; j < arr2.length;j++){
+          
+          if ( arr1[i] == arr2[j] ){
+            similars.push(arr1[i])
+          }
+          
+        }
+
+}
+return similars
+}
+
+let arrOne = [1,2,3,4,5,6,7,8]
+let arrTwo = [4,5,8]
+
+console.log(similarElements(arrOne,arrTwo)) // OUTPUT - [4,5,8]
+
+// ---------------------------------------------------------------------------------------------------
+
+
+
 
 
 
